ae3e4a2b98d1a1b0423199f17590b4cffab40d63
Camila <camilahaddad@gmail.com>
2024-03-04 18:56:08 -0300

    Subindo API

---
+++ web-api/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System> web_api.Http.Cors;
+
+namespace web_api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuração e serviços de API Web
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.
+
+            // Rotas de API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
